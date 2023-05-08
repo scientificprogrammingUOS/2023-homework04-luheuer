f8f0e19490a88407526b8f0be9edda6d263eaf00
luheuer <102998800+luheuer@users.noreply.github.com>
2023-05-09 00:05:21 +0200

    Update strange_pattern.py

--- strange_pattern.py
+++ strange_pattern.py
@@ -2,9 +2,14 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def strange_pattern(shape):
+    pattern = np.full(shape= shape, fill_value= False)
+   
+    for i in range(0,shape[0]):
+        for k in range(i%3,shape[1],3):
+            pattern[i,k ] = True
+
+  return pattern
 
 
 if __name__ == "__main__":
